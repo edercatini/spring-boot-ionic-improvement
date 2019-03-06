365956a52567f53096e22b6c9fc65207a2dd4bbc
Eder Catini <edergcatini@icloud.com>
2019-03-05 22:21:54 -0300

    Commit inicial

---
+++ src/main/java/com/edercatini/spring/SpringBootIonicApplication.java
@@ -0,0 +1,13 @@
+package com.edercatini.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicApplication.class, args);
+	}
+
+}
